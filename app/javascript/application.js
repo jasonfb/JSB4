5119b9adb5485b497160114626e6d77ca7fb40d6
Jason Fleetwood-Boldt <jason@heliosdev.shop>
2023-09-20 16:54:13 -0400

    does not work

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,10 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+
+
+import MyComponent from "./components/MyComponent"
+
+const myComponent = new MyComponent();
+
+document.querySelector("#my-app").innerHTML = myComponent.render();
